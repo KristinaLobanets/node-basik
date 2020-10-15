65648b9a4bc1e5c587230fc2b8c3577ad5e7aa09
Kristina Lobanets <kristina.malitowska@gmail.com>
2020-10-15 22:26:22 +0300

    jjj

--- user/userControler.js
+++ user/userControler.js
@@ -1,7 +1,77 @@
 const contacts = require("../db/contacts.json");
+const Joi = require("joi");
 
 module.exports = class UserController {
   static listContacts(req, res, next) {
     res.status(200).json(contacts);
+  }
+
+  static getById(req, res, next) {
+    const id = Number(req.params.contactId);
+
+    const user = contacts.find((item) => item.id === id);
+    if (user === undefined) {
+      res.status(404).send("Not found");
+    }
+    res.status(200).json(user);
+  }
+
+  static AddContact(req, res, next) {
+    const newContact = { ...req.body, id: contacts.length + 1 };
+
+    contacts.push(newContact);
+    res.status(201).json(contacts);
+  }
+
+  static DeleteContact(req, res, next) {
+    const id = Number(req.params.contactId);
+    const userIndex = contacts.findIndex((item) => item.id === id);
+    if (userIndex === -1) {
+      res.status(404).send("Not found");
+    }
+    contacts.splice(userIndex, 1);
+    res.status(200).send("contact deleted");
+  }
+
+  static updateContact(req, res, next) {
+    const id = Number(req.params.contactId);
+    const userIndex = contacts.findIndex((item) => item.id === id);
+    if (userIndex === -1) {
+      res.status(404).send("Not found");
+    }
+    contacts[userIndex] = { ...contacts[userIndex], ...req.body };
+    res.status(200).json(contacts[userIndex]);
+  }
+
+  static validateAddContact(req, res, next) {
+    const createValidationRules = Joi.object({
+      nane: Joi.string().required(),
+      email: Joi.string().required(),
+      phone: Joi.string().required(),
+    });
+
+    const result = createValidationRules.validate(req.body);
+    if (result.error) {
+      return res.status(400).send(result.error);
+    }
+    next();
+  }
+
+  static validateUpdateContact(req, res, next) {
+    const createValidationRules = Joi.object({
+      nane: Joi.string(),
+      email: Joi.string(),
+      phone: Joi.string(),
+    });
+
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).send("Missing filds");
+    }
+
+    const result = createValidationRules.validate(req.body);
+    if (result.error) {
+      return res.status(400).send(result.error);
+    }
+    next();
   }
 };
--- user/userRouter.js
+++ user/userRouter.js
@@ -4,4 +4,21 @@ const UserController = require("./userControler");
 const UserRouter = Router();
 
 UserRouter.get("/contacts", UserController.listContacts);
+
+UserRouter.get("/contacts/:contactId", UserController.getById);
+
+UserRouter.post(
+  "/contacts",
+  UserController.validateAddContact,
+  UserController.AddContact
+);
+
+UserRouter.delete("/contacts/:contactId", UserController.DeleteContact);
+
+UserRouter.patch(
+  "/contacts/:contactId",
+  UserController.validateUpdateContact,
+  UserController.updateContact
+);
+
 module.exports = UserRouter;
